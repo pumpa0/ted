a0d4cb709af1aaaa4be4f8c7a130503cd655c79d
pumpa0 <87925130+pumpa0@users.noreply.github.com>
2022-03-31 23:13:07 +0700

    Add files via upload

--- hisoka.js
+++ hisoka.js
@@ -1960,58 +1960,38 @@ break
                 }
             }
             break
-	        case 'ttnowm': case 'tiktoknowm': {
+	        case 'tiktok': case 'tt': {
                 if (!text) throw 'Masukkan Query Link!'
-                let anu = await fetchJson(api('zenz', '/downloader/tiktok', { url: text }, 'apikey'))
+                
                 let buttons = [
                     {buttonId: `tiktokwm ${text}`, buttonText: {displayText: '► With Watermark'}, type: 1},
-                    {buttonId: `tiktokmp3 ${text}`, buttonText: {displayText: '♫ Audio'}, type: 1}
-                ]
-                let buttonMessage = {
-                    video: { url: anu.result.nowatermark },
-                    caption: `Download From ${text}`,
-                    footer: 'Press The Button Below',
-                    buttons: buttons,
-                    headerType: 5
-                }
-                hisoka.sendMessage(m.chat, buttonMessage, { quoted: m })
-            }
-            break
-            case 'tiktok': case 'tiktokwm': {
-                if (!text) throw 'Masukkan Query Link!'
-                let anu = await fetchJson(api('zenz', '/downloader/tiktok', { url: text }, 'apikey'))
-                let buttons = [
                     {buttonId: `tiktoknowm ${text}`, buttonText: {displayText: '► No Watermark'}, type: 1},
                     {buttonId: `tiktokmp3 ${text}`, buttonText: {displayText: '♫ Audio'}, type: 1}
                 ]
-                let buttonMessage = {
-                    video: { url: anu.result.watermark },
-                    caption: `Download From ${text}`,
-                    footer: 'Press The Button Below',
-                    buttons: buttons,
-                    headerType: 5
-                }
-                hisoka.sendMessage(m.chat, buttonMessage, { quoted: m })
-            }
+                await hisoka.sendButtonText(m.chat, buttons, `\`\`\`PILIH JENIS FILE DIBAWAH INI!\`\`\``)
+         
+            break
+            case 'tiktokwm': {
+                if (!text) throw 'Masukkan Query Link!'
+                let ttwm = await fetchJson(`https://zenzapi.xyz/downloader/tiktok?url=${text}&apikey=0af8c9e45d`)
+            wmtt = (`${ttwm.result.watermark}`)
+                await hisoka.sendMessage(m.chat, { video: { url: wmtt, caption: `Tiktok From ${text}` } }, { quoted: m })
+break
+case 'tiktoknowm': {
+                if (!text) throw 'Masukkan Query Link!'
+                let ttnowm = await fetchJson(`https://zenzapi.xyz/downloader/tiktok?url=${text}&apikey=0af8c9e45d`)
+            wmnott = (`${ttnowm.result.nowatermark}`)
+                await hisoka.sendMessage(m.chat, { video: { url: wmnott, caption: `Tiktok From ${text}` } }, { quoted: m })
+break
             break
             case 'tiktokmp3': case 'tiktokaudio': {
                 if (!text) throw 'Masukkan Query Link!'
                 let anu = await fetchJson(api('zenz', '/downloader/tiktok', { url: text }, 'apikey'))
-                let buttons = [
-                    {buttonId: `tiktoknowm ${text}`, buttonText: {displayText: '► No Watermark'}, type: 1},
-                    {buttonId: `tiktokwm ${text}`, buttonText: {displayText: '► With Watermark'}, type: 1}
-                ]
-                let buttonMessage = {
-                    text: `Download From ${text}`,
-                    footer: 'Press The Button Below',
-                    buttons: buttons,
-                    headerType: 2
-                }
-                let msg = await hisoka.sendMessage(m.chat, buttonMessage, { quoted: m })
+                
 		let { toAudio } = require('./lib/converter')
 		let nganu = await getBuffer(anu.result.nowatermark)
 		let cnvrt = await toAudio(nganu, 'mp4')
-                hisoka.sendMessage(m.chat, { audio: cnvrt, mimetype: 'audio/mpeg'}, { quoted: msg })
+                hisoka.sendMessage(m.chat, { audio: cnvrt, mimetype: 'audio/mpeg'}, { quoted: m })
             }
             break
 	        case 'instagram': case 'ig': case 'igdl': {
@@ -2511,18 +2491,7 @@ ${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Obje
                 hisoka.sendContact(m.chat, global.owner, m)
             }
             break
-case 'coba':
-if (!text) throw 'Masukkan Query Link!'
-                let anu = await fetchJson(`https://zenzapi.xyz/downloader/tiktok?url=${text}&apikey=0af8c9e45d`)
-            hasill = (`${anu.result.nowatermark}`)
-                m.reply(hasil)
-break
-case 'coba2':
-if (!text) throw 'Masukkan Query Link!'
-                let anu2 = await fetchJson(`https://zenzapi.xyz/downloader/tiktok?url=${text}&apikey=0af8c9e45d`)
-            hasill2 = (`${anu2.result.nowatermark}`)
-                await hisoka.sendMessage(m.chat, { video: { url: hasill2, caption: `Tiktok From ${text}` } }, { quoted: m })
-break
+
             case 'list': case 'menu': case 'help': case '?': {
                 anu = `┌──⭓ *Group Menu*
 │
