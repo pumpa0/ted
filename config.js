793a51e2a32a61e123f2b39fd4232fe85d5a94f4
pumpa0 <pumpa0pumpa@gmail.com>
2022-03-31 17:05:47 +0700

    ted

---
+++ config.js
@@ -0,0 +1,51 @@
+/**
+   * Create TedyBotz.
+   * Contact Me on wa.me/6281271007021
+   * Follow https://github.com/TedyBotz
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapi.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapi.xyz': '0af8c9e45d',
+}
+
+// Other
+global.owner = ['6285731855426']
+global.premium = ['6285731855426']
+global.packname = 'Han'
+global.author = '@terserah_bomat'
+global.sessionName = 'session'
+global.prefa = ['','!','.','🐦','🐤','🗿','/','#']
+global.sp = '•'
+global.mess = {
+    success: 'Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./lib/hisoka.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
