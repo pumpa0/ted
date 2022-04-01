0ab0bfc84772bf55f082c45053fb82011f2b3f08
pumpa0 <87925130+pumpa0@users.noreply.github.com>
2022-04-01 20:11:29 +0700

    Add files via upload

--- hisoka.js
+++ hisoka.js
@@ -2013,7 +2013,7 @@ case 'tiktok': case 'tiktoknowm': {
                     headerType: 2
                 }
                 let msg = await hisoka.sendMessage(m.chat, buttonMessage, { quoted: m })
-                hisoka.sendMessage(m.chat, { audio: anu }, mimetype: 'audio/mpeg'}, { quoted: msg })
+                hisoka.sendMessage(m.chat, { audio: { anu }, mimetype: 'audio/mpeg'}, { quoted: msg })
             }
             break
 	        case 'instagram': case 'ig': case 'igdl': {
