a13bcec25bdb3245dc3fd1f26bbd831d14ab7a85
pumpa0 <87925130+pumpa0@users.noreply.github.com>
2022-04-03 21:20:59 +0700

    Add files via upload

--- hanbotz.js
+++ hanbotz.js
@@ -2852,7 +2852,7 @@ ${readmore}
         
 
     } catch (err) {
-        m.reply(util.format(err)\n\nReport Ke Owner\nwa.me/6285731855426)
+        m.reply(`util.format(err)\n\nReport Ke Owner\nwa.me/6285731855426`)
     }
 }
 
